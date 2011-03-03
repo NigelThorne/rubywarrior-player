a7bf7284ad10c0e940550988997b6fe5f4eea73b
Merlyn Albery-Speyer <curious.attempt.bunny@gmail.com>
2011-03-02 21:19:39 -0800

    completed level 9

--- merlyn-beginner/player.rb
+++ merlyn-beginner/player.rb
@@ -1,4 +1,9 @@
 class Player
+  def initialize()
+  	@prior_health = 20
+  	@health = 20
+  end
+  
   def play_turn(warrior)
     if warrior.respond_to?(:health)
       @health = warrior.health
@@ -8,17 +13,42 @@ class Player
     if warrior.respond_to?(:feel)
       @empty = warrior.feel.empty?
       @captive = warrior.feel.captive?
-      puts warrior.feel.to_s
       @enemy_archer = warrior.feel.to_s == 'Archer'
+      @wall = warrior.feel.wall?
     else
       @empty = true
 	  @captive = false
+	  @wall = false
+	  @enemy_archer = false
+    end
+    if warrior.respond_to?(:look)
+      @first_thing = warrior.look.find {|space| !space.empty? }
+      @shootable = false
+      if @first_thing != nil && @first_thing.to_s != 'wall'
+        @shootable = !@first_thing.captive?
+      end
     end
     act(warrior)
     @prior_health = @health
   end
   
   def act(warrior)
+    if @next_action == :pivot
+      @next_action = nil
+      warrior.pivot!
+      return
+    end
+    
+    if @health < @prior_health && @empty && @first_thing != nil && @first_thing.to_s != 'Archer'
+      warrior.pivot!
+      return
+    end
+    
+    if @shootable
+      warrior.shoot!
+      return
+    end
+    
   	if @empty && @health < 20 && @prior_health <= @health
   	  warrior.rest!
   	  return
@@ -37,6 +67,18 @@ class Player
     if @empty
       warrior.walk!
       return
+	end
+	
+	if @wall
+	  warrior.pivot!
+	  return
+	end
+	
+	if !@enemy_archer && !@pivotted
+	  @pivotted = true
+	  warrior.walk!(:backward)
+	  @next_action = :pivot
+	  return
 	end
 	
     warrior.attack!
